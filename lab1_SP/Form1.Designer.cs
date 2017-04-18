1e4398c0e451c75c87361cf5bcca59e09f395700
Vlad <Vlad@example>
2017-04-18 17:09:42 +0300

    Добавьте файлы проекта.

---
+++ lab1_SP/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace lab1_SP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chartSignal = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbN = new System.Windows.Forms.TextBox();
+            this.chartAmplitude = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSignal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAmplitude)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(382, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Спектральный анализ периодических сигналов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Погрешность:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0,95";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chartSignal
+            // 
+            this.chartSignal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea5.Name = "ChartArea1";
+            this.chartSignal.ChartAreas.Add(chartArea5);
+            legend5.Alignment = System.Drawing.StringAlignment.Center;
+            legend5.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend5.Name = "Legend1";
+            this.chartSignal.Legends.Add(legend5);
+            this.chartSignal.Location = new System.Drawing.Point(155, 30);
+            this.chartSignal.Name = "chartSignal";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series9.Legend = "Legend1";
+            series9.MarkerBorderColor = System.Drawing.Color.White;
+            series9.Name = "Исходный сигнал";
+            series9.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series9.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series10.Legend = "Legend1";
+            series10.Name = "Полученный сигнал";
+            this.chartSignal.Series.Add(series9);
+            this.chartSignal.Series.Add(series10);
+            this.chartSignal.Size = new System.Drawing.Size(587, 255);
+            this.chartSignal.TabIndex = 4;
+            this.chartSignal.Text = "chart1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Количество\r\nгармоник:";
+            // 
+            // tbN
+            // 
+            this.tbN.Location = new System.Drawing.Point(96, 96);
+            this.tbN.Name = "tbN";
+            this.tbN.ReadOnly = true;
+            this.tbN.Size = new System.Drawing.Size(53, 20);
+            this.tbN.TabIndex = 9;
+            // 
+            // chartAmplitude
+            // 
+            this.chartAmplitude.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea6.Name = "ChartArea1";
+            this.chartAmplitude.ChartAreas.Add(chartArea6);
+            legend6.Alignment = System.Drawing.StringAlignment.Center;
+            legend6.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend6.Name = "Legend1";
+            this.chartAmplitude.Legends.Add(legend6);
+            this.chartAmplitude.Location = new System.Drawing.Point(155, 291);
+            this.chartAmplitude.Name = "chartAmplitude";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series11.Legend = "Legend1";
+            series11.LegendText = "Амплитуда";
+            series11.Name = "Series1";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series12.Legend = "Legend1";
+            series12.LegendText = "Фаза";
+            series12.Name = "Series2";
+            this.chartAmplitude.Series.Add(series11);
+            this.chartAmplitude.Series.Add(series12);
+            this.chartAmplitude.Size = new System.Drawing.Size(587, 255);
+            this.chartAmplitude.TabIndex = 10;
+            this.chartAmplitude.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 134);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 63);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Исходные данные";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Emax = 5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "tи = 116";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "T = 232";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 553);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chartAmplitude);
+            this.Controls.Add(this.tbN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chartSignal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа № 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartSignal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAmplitude)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSignal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbN;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartAmplitude;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab1_SP/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_SP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int n = 0;
+        const int Emax = 5;
+        const double a0 = Emax;
+        const int ti = 116; //длительность импульсов
+        const int T = 232;  //период
+        
+
+        public static void myMethod(double err)
+        {
+            double Pc = Math.Pow(Emax, 2) / 2;
+            double Pf = 0;
+            double sumAn = 0;
+            double An;
+
+            while (Pf / Pc <= err)
+            {
+                n++;
+                Pf = Math.Pow(a0, 2) / 4;
+                sumAn = 0;
+                
+                for (int i = 1; i <= n; i++)
+                {
+                    An = Math.Sqrt(Math.Pow(Emax / (i * Math.PI) * (1 - Math.Cos(i * Math.PI)), 2));
+                    sumAn += Math.Pow(An, 2);
+                }
+                sumAn /= 2;
+                Pf += sumAn;
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = 0;
+            
+            chartSignal.Series[1].Points.Clear();
+            chartAmplitude.Series[0].Points.Clear();
+            chartAmplitude.Series[1].Points.Clear();
+
+            double error;
+            if(!double.TryParse(textBox1.Text, out error))
+            {
+                MessageBox.Show("Error in parse double");
+                return;
+            }
+            myMethod(error);
+
+            double[] arr_bn = new double[n];
+            double[] arr_St = new double[ti];
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+                arr_bn[i] = Emax / ((i + 1) * Math.PI) * (1 - Math.Cos((i + 1) * Math.PI));
+
+            for (int i = 0; i < ti; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += arr_bn[j] * Math.Sin((j + 1) * 2 * Math.PI / T * i);
+                }
+                arr_St[i] = a0 / 2 + sum;
+
+                chartSignal.Series[1].Points.AddXY(i, arr_St[i]);
+            }
+
+            chartAmplitude.ChartAreas[0].AxisX.Minimum = 0;
+            chartAmplitude.ChartAreas[0].AxisX.Maximum = n - 1;
+            chartAmplitude.Series[1].Color = Color.BlueViolet;
+
+            double[] An = new double[n];
+            double[] Fi = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                An[i] = Emax / ((i + 1) * Math.PI) * (1 - Math.Cos((i + 1) * Math.PI));
+                Fi[i] = Math.PI / 2;
+                chartAmplitude.Series[1].Points.AddXY(i, Fi[i]);
+                chartAmplitude.Series[0].Points.AddXY(i, An[i]);
+            }            
+
+            tbN.Text = n.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chartSignal.Series[1].Color = Color.Red;
+            chartSignal.Series[0].Color = Color.Black;
+            chartSignal.Series[0].BorderWidth = 2;
+            chartSignal.Series[1].BorderWidth = 2;
+            chartSignal.ChartAreas[0].AxisX.Minimum = -ti / 2;
+            chartSignal.ChartAreas[0].AxisX.Maximum = T - ti / 2;
+
+            chartSignal.Series[0].Points.AddXY(-ti, 0);
+            chartSignal.Series[0].Points.AddXY(0, 0);
+            chartSignal.Series[0].Points.AddXY(0, Emax);
+            chartSignal.Series[0].Points.AddXY(ti, Emax);
+            chartSignal.Series[0].Points.AddXY(ti, 0);
+            chartSignal.Series[0].Points.AddXY(T, 0);
+        }
+    }
+}
